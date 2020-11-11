85f09e471bae0c90886d6cf127535586c42f9fff
doer-123123 <whs724670589@live.com>
2020-11-11 16:19:17 +0800

    https://github.com/doer-123123/git-test.git

--- chapter0/main.c
+++ chapter0/main.c
@@ -1,7 +1,30 @@
-#include <stdio.h>
 
-int main(void)
-{
-    printf("Hello Cacu!\n");
-    return 0;
-}
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# On branch main
+# Your branch is up to date with 'origin/main'.
+#
+# Changes to be committed:
+#	new file:   chapter0/add_minus.c
+#	new file:   chapter0/add_minus.h
+#	new file:   chapter0/main.c
+#	new file:   chapter0/main.o
+#	new file:   chapter0/multi_div.c
+#	new file:   chapter0/multi_div.h
+#	new file:   chapter0/v1.0.patch
+#	new file:   chapter0/v2.0.patch
+#	new file:   chapter0/v3.0.patch
+#
+# Untracked files:
+#	chapter1/
+#	chapter10/
+#	chapter2/
+#	chapter3/
+#	chapter4/
+#	chapter5/
+#	chapter6/
+#	chapter7/
+#	chapter8/
+#	chapter9/
+#
